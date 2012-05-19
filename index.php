55afcc21eada84a39352768d714dd4801bda6da7
Ravee Malla <ravmalla@gmail.com>
2012-05-19 14:00:42 +0530

    message

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  /*
+   * Version 1. IIT Debates is a site to facilitate meaningful dialogue in
+   * between acquintances on issues of mutual concern. I am more likely to be
+   * interested in things that my friends are interested in, and I will be
+   * more interested in their opinions.
+   * Give users the optiond of
+   * 1. starting a debate
+   * 2. following a debate
+   * 3. follow a debater
+   * 4. rate debates, debaters (implicitly)
+   * For any debate, the user will have the option of starting a new point, 
+   * rebutting a point, supporting a point
+   * Each debate has a time frame and then the one with the most points wins (?)
+   */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>IIT Debates</title>
+<script src="jquery.min.js" type="text/javascript"></script>
+<link rel="stylesheet" href="includes/style.css"/>
+</head>
+<body>
+<h1 class="logo">IIT Debates</h1>
+</body>
+</html>
